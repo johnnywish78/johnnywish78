311bd03f37d252d5fb0f6948e57a4b131f922d66
Johnny Wish <johnnywish78@gmail.com>
2026-06-24 09:07:27 +0330

    first animated text

---
+++ text.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+text = "HELLO LINUX"
+
+while True:
+    os.system("clear")
+
+    for i in range(len(text) + 1):
+        print(text[:i])
+        time.sleep(0.1)
+
+    time.sleep(0.5)
+
+    for i in range(len(text), -1, -1):
+        print(text[:i])
+        time.sleep(0.1)
